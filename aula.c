132745f231480d23583a680c9bc43b74ef218840
lunahalabi <lunafeitosa2006@gmail.com>
2026-08-31 22:27:17 -0300

    Update aula.c

--- aula.c
+++ aula.c
@@ -76,3 +76,47 @@ int main(int argc, char *argv[]) {
 	
 	return 0;
 }
+
+
+
+
+int main(int argc, char *argv[]) {
+	int n;
+		printf("entre com o numero: ");
+	scanf("%d", &n);
+	// equivalente (n>= 1 && n <= 9)
+	if(n<10 && n>0)
+		// isso é interessante (!(n == 4 | | n == 6 | | n == 8 | | n == 9))
+	if(n == 1 || n == 2 | | n == 3| | n == 5 | | n == 7){
+		printf("o dobro de %d = %d", n, (n*2));
+	}else if(n%2 == 0)
+		printf("%d+2 =%d", n, n+2);
+	else
+		printf("|%d|%d|%d|", n-1, n, n+1);
+
+	else printf("_out_of_range");
+	
+	
+	char op;
+	scanf("%c", &letra);
+	if(letra == 'a')printf("a de amor");
+	else if (letra == 'b')printf("b de baixinho da xuxa");
+	else if (letra == 'c')printf("c de coração");
+
+
+
+	char op;
+	scanf("%c", &op);
+	switch(op){
+		
+	case 'a': printf("opa ta falando com ele"); break;
+	
+	case 'b': printf("Benedito!"); break;
+	
+	case 'c': printf("É complicada a situação!"); break;
+	
+	case 'd': printf("Tamo junto"); break;
+	
+	}
+return 0; 
+}
